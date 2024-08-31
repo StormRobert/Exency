646115758c2e3884a95b806f16d90801b96abf27
StormRobert <stormarasa2@gmail.com>
2024-08-31 06:40:41 +0300

    finished converter finally

--- src/components/transaction&conversions/currencyconverter.jsx
+++ src/components/transaction&conversions/currencyconverter.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AiOutlineSwap } from 'react-icons/ai'
 import Currencyselect from './currencyselect'
 import { useState } from 'react'
@@ -7,7 +7,9 @@ import { useState } from 'react'
 const currencyconverter = () => {
     const [fromCurrency, setFromCurrency] = useState('USD')
     const [toCurrency, setToCurrency] = useState('KES')
+    const [result, setResult] = useState("")
     const [amount, setAmount] = useState(100)
+    const [loading, setLoading] = useState(false)
 
     const handleSwapCurrency = () => {
         setFromCurrency(toCurrency)
@@ -18,23 +20,29 @@ const currencyconverter = () => {
     const getExchangeRate = async() => {
         const API_KEY = "cff92de5e4f162e0ec9a1668";
         const API_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/pair/${fromCurrency}/${toCurrency}`
+        setLoading(true)
 
         try{
             const response = await fetch(API_URL)
             if(!response.ok) throw Error("Someting wnt wrong!")
                 const data = await response.json()
                 console.log(data)
-            const exchangeRate = (data.conversion_rate * amount).toFixed();
+            const exchangeRate = (data.conversion_rate * amount).toFixed(2);
             console.log(exchangeRate)
+            setResult(`${amount} ${fromCurrency} =${exchangeRate} ${toCurrency}`)
 
         } catch(error){
             console.log(error)
+        } finally{
+            setLoading(false)
         }
     }
     const handleFormSubmit = (e) => {
         e.preventDefault()
         getExchangeRate()
     }
+
+    useEffect(() => getExchangeRate, [])
 
   return (
     <div className="flex h-screen items-center justify-center ">
@@ -69,9 +77,16 @@ const currencyconverter = () => {
             />
           </div>
         </div>
-        <button type='submit' className='w-full px-4 py-2 mt-6 font-bold text-white bg-cyan-500 rounded hover:bg-cyan-700 focus:outline-none focus:shadow-outline transition-colors duration-300'>Current Rates</button>
-        <p className="text-center text-cyan-300 mt-4 exchangerateresult"></p>
+        <button type='submit' className='w-full px-4 py-2 mt-6 font-bold text-white bg-cyan-500 del rounded hover:bg-cyan-700 focus:outline-none focus:shadow-outline transition-colors duration-300'
+        
+        >Current Rates</button>
+        {/* <p className="text-center text-cyan-300 mt-4 ">{result}</p> */}
       </form>
+      {result && (
+                    <div className="mt-6 p-4 bg-neutral-800 rounded-lg shadow-inner text-center text-cyan-300 delay-300 ">
+                        {loading ? "Getting the exchange rate..." : result}
+                    </div>
+                )}
     </div>
   </div>
   )
