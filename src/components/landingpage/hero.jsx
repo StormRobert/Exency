54a1a817a63d2bedce28340d9bf3321bf3551017
StormRobert <stormarasa2@gmail.com>
2024-08-31 16:39:38 +0300

    edits

--- src/components/landingpage/hero.jsx
+++ src/components/landingpage/hero.jsx
@@ -11,13 +11,17 @@ const Hero = () => {
     navigate('/signup');
   };
 
+  const handleLoginClick = () => {
+    navigate('/login');
+  }
+
   return (
     <div className='border-b border-neutral-800 pb-16 lg:mb-35'>
         <div className='flex flex-wrap'>
             <div className='w-full -my-6 lg:w-1/2'>
             <div className='flex flex-col items-center lg:items-start'>
-                <span className="bg-gradient-to-r from-purple-400 via-slate-400 to-indigo-400 bg-clip-text text-3xl lg:text-4xl tracking-tight text-transparent">Seamless Currency Conversion</span>
-                <p className='my-2 mx-6 py-6 font-light tracking-tight'>
+                <span className="bg-gradient-to-r from-purple-400 via-slate-400 to-indigo-400 bg-clip-text text-3xl lg:text-6xl tracking-tight text-transparent">Seamless Currency Conversion</span>
+                <p className='my-2 mx-6 py-6 font-light tracking-tight lg:text-2xl '>
                     {HERO_CONTENT}
                 </p>
             </div>
@@ -29,15 +33,22 @@ const Hero = () => {
             </div>
         </div>
         <button
-            className="w-full px-4 py-2 font-bold text-white bg-cyan-500 rounded hover:bg-cyan-700 focus:outline-none focus:shadow-outline"
+            className="w-1/12 l:w-24 mx-5 px-4 py-3 font-bold text-white bg-cyan-500 rounded hover:bg-cyan-700 focus:outline-none focus:shadow-outline"
             type="button"
             onClick={handleSignUpClick}
         >
             Sign Up
         </button>
-        <div className='w-full mx-10 my-10 px-10 flex items-center justify-center'>
-                <div className='p-4'>
-                    <h3 className='text-2xl font-semibold mb-4 text-white'>Instant Currency Conversion</h3>
+        <button
+            className="w-1/12 l:w-24 px-4 py-3 font-bold text-white bg-cyan-500 rounded hover:bg-cyan-700 focus:outline-none focus:shadow-outline"
+            type="button"
+            onClick={handleLoginClick}
+        >
+            Login
+        </button>
+        <div className='w-1/2 mx-10 my-10 px-10 flex flexwrap items-center justify-center'>
+                <div className='p-4 '>
+                    <h3 className='text-2xl my-4 font-semibold mt- mb-6 text-white'>Instant Currency Conversion</h3>
                     <p className='text-lg text-neutral-300 mb-6'>
                         Experience hassle-free currency conversion with our intuitive tool. Easily convert between multiple currencies in real-time, and enjoy seamless transactions no matter where you are. Our currency converter ensures you get the best exchange rates with minimal fees, making it perfect for travelers and businesses alike.
                     </p>
